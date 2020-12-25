88d56546ecda4be093036b0d589e4a4d6442f231
ricoter <rico.vanmidde@gmail.com>
2020-12-25 22:09:55 +0100

    17 short version

--- day17short.jl
+++ day17short.jl
@@ -1,31 +1,22 @@
 """day17 short version"""
 data = permutedims(hcat(map(collect, readlines("input/17"))...)) .== '#' # 2D BitArray
-cycles = 6
-l = size(data)[1]
-xmax = l + 2cycles# init
+
+l = size(data)[1]  # data length
+xmax = l + 12      # data length + 2 sweeps
 
 function play(n)
-    grid = falses([xmax for _=1:n]...)              # init n dimensions with zeros
-    grid[7:6+L,7:6+L, [10 for _=1:n-2]...] = data   # place data in the centre
-    r(i) = max(1, i-1):min(i+1, xmax)               # neighbors
-    neighbors(x) = sum(grid[r.(x)...]) - grid[x...] # active neightbors
+    prev = grid = falses([xmax for _=1:n]...)       # nd BitArray with 0's
+    grid[7:6+l,7:6+l, [10 for _=1:n-2]...] = data   # copy data to centre
 
-    for _ in 1:cycles
-        M = copy(grid)
-        M[grid .== 1] = map(x -> x ∈ [2,3], neighbors.(Tuple.(findall(grid .== 1))))
-        M[grid .== 0] = neighbors.(Tuple.(findall(grid .== 0))) .== 3
-        grid = M
+    r(i) = max(1, i-1):min(i+1, xmax)               # first neighbors within boundary
+    neighbors(x) = sum(prev[r.(x)...]) - prev[x...] # active neightbors
+
+    for _ in 1:6            
+        prev = copy(grid)
+        grid[prev .== 1] = in.(neighbors.(Tuple.(findall(prev .== 1))), [[2,3]]) # update 1's
+        grid[prev .== 0] = in.(neighbors.(Tuple.(findall(prev .== 0))), [3])     # update 0's
     end
     
-    return sum(grid)
+    return sum(grid) # count 1's
 end
-@time play.([3,4])
-
-
-    # function cycle(M, i)
-    #     M[grid .== 1] = map(x -> x ∈ [2,3], neighbors.(Tuple.(findall(grid .== 1))))
-    #     M[grid .== 0] = neighbors.(Tuple.(findall(grid .== 0))) .== 3
-    #     return i < 1 ? cycle(grid=copy(M), 1), M
-    # end
-
-    # return sum([(grid = cycle(copy(grid), 6)) for _=1:6][end])
+@time play.([3,4]) # run 3D and 4D
