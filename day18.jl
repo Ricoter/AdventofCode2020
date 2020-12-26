3cf8a20b56eb8de8f5a28b881ff237d417341ece
ricoter <rico.vanmidde@gmail.com>
2020-12-26 03:50:51 +0100

    day18 short

--- day18.jl
+++ day18.jl
@@ -1,36 +1,13 @@
-#=
-Advent of Code 2020, Day 18
-Julia v1.5.3
-Rico van Midde
-=#
+"""Day 18 short version"""
 
-# read input
 data = readlines("input/18")
 
-# show some data
-println(data[1])
-
+# For more information see this discussion about operation precedence
 # https://discourse.julialang.org/t/on-adjoints-and-custom-postfix-and-infix-operators/12863/35
-# change + to other Unicode symbol with the same precedence (11) as *
-⦾(a,b) = +(a,b)
 
-x = 0
-for line in data
-    line = split(line, "")
-    replace!(line, "+" => "⦾")
-    line = join(line)
-    x += eval(Meta.parse(line)) # evaluate string
-end
-println("Part One: $x")
+⦾(a,b) = +(a,b) # change + to Unicode symbol with same precedence (11) as *
+⟱(a,b) = +(a,b) # change + to Unicode symbol with more precedence (15) than * (same as ^)
 
-# change * to other Unicode symbol with the same precedence (12) as +
-⋓(a,b) = *(a,b)
-
-x = 0
-for line in data
-    line = split(line, "")
-    replace!(line, "+" => "⦾", "*" => "⋓")
-    line = join(line)
-    x += eval(Meta.parse(line))
-end
-println("Part Two: $x")
+# sum all evaluated lines with customized precedence
+CustomizedPrecedence(symbol) = sum([eval(Meta.parse(replace(line, r"\+" => symbol))) for line in data])
+CustomizedPrecedence.(["⦾", "⟱"])
