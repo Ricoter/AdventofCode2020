4858fc06d15c3cba8bea4313099dd97a90886147
ricoter <rico.vanmidde@gmail.com>
2020-12-26 02:43:19 +0100

    Day 18

--- day17short.jl
+++ day17short.jl
@@ -1,12 +1,12 @@
 """day17 short version"""
 data = permutedims(hcat(map(collect, readlines("input/17"))...)) .== '#' # 2D BitArray
 
-l = size(data)[1]  # data length
-xmax = l + 12      # data length + 2 sweeps
+l = size(data)[1] # data length
+xmax = l + 12     # data length + 2 sweeps
 
 function play(n)
-    prev = grid = falses([xmax for _=1:n]...)       # nd BitArray with 0's
-    grid[7:6+l,7:6+l, [10 for _=1:n-2]...] = data   # copy data to centre
+    prev = grid = falses([xmax for _=1:n]...)         # nd BitArray with 0's
+    grid[7:6+l,7:6+l, [xmax÷2 for _=1:n-2]...] = data # copy data to centre
 
     r(i) = max(1, i-1):min(i+1, xmax)               # first neighbors within boundary
     neighbors(x) = sum(prev[r.(x)...]) - prev[x...] # active neightbors
--- day18.jl
+++ day18.jl
@@ -8,4 +8,29 @@ Rico van Midde
 data = readlines("input/18")
 
 # show some data
-println(data[begin:3])
+println(data[1])
+
+# https://discourse.julialang.org/t/on-adjoints-and-custom-postfix-and-infix-operators/12863/35
+# change + to other Unicode symbol with the same precedence (11) as *
+⦾(a,b) = +(a,b)
+
+x = 0
+for line in data
+    line = split(line, "")
+    replace!(line, "+" => "⦾")
+    line = join(line)
+    x += eval(Meta.parse(line)) # evaluate string
+end
+println("Part One: $x")
+
+# change * to other Unicode symbol with the same precedence (12) as +
+⋓(a,b) = *(a,b)
+
+x = 0
+for line in data
+    line = split(line, "")
+    replace!(line, "+" => "⦾", "*" => "⋓")
+    line = join(line)
+    x += eval(Meta.parse(line))
+end
+println("Part Two: $x")
