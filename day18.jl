06d6e6528ac1c800d643cde1733dbfb918383d97
ricoter <rico.vanmidde@gmail.com>
2020-12-27 03:37:49 +0100

    day 19

--- day18.jl
+++ day18.jl
@@ -5,6 +5,9 @@ data = readlines("input/18")
 # For more information see this discussion about operation precedence
 # https://discourse.julialang.org/t/on-adjoints-and-custom-postfix-and-infix-operators/12863/35
 
+# For a list of Unicode symbols that have the same precedence see
+# https://github.com/JuliaLang/julia/blob/a882d9bf183fc93319002c9158f406f0755c202e/src/julia-parser.scm#L29
+
 ⦾(a,b) = +(a,b) # change + to Unicode symbol with same precedence (11) as *
 ⟱(a,b) = +(a,b) # change + to Unicode symbol with more precedence (15) than * (same as ^)
 
---
+++ day19.jl
@@ -0,0 +1,45 @@
+#=
+Advent of Code 2020, Day 19
+Julia v1.5.3
+Rico van Midde
+=#
+
+SpecialChars = ["a", "b", "(", ")", "|"]            # chars for regex
+part = 1                                            # keep track of part
+
+data = readlines("input/19")                        # read data
+cut = findfirst(data .== "")                        # cut data at empty line
+
+rules = Dict{Any, Any}(x=>x for x in SpecialChars)  # rules
+messages = data[cut+1:end]                          # messages
+
+for line in data[begin:cut-1]                       # fill dict with all rules
+    n, r = split(line, ": ")
+    rules[n] = (r[1] == '"') ? r[2] : [["("] ; split(r) ; [")"]]
+end
+
+function codes(key)
+    x = rules[string(key)]
+
+    if x ∈ SpecialChars        # return regex chars
+        return x
+
+    elseif part==2 && key=="8" # special rules for part two
+        return codes(42)*"+"
+    elseif part==2 && key=="11"
+        return "(?P<x>$(codes(42))(?&x)?$(codes(31)))" # regex recursion with named groups
+        # return codes(42)*"(?1)?"*codes(31) # regex recursion with numbered groups
+
+    else                                   
+        return join(codes.(x)) # increase depth
+    end
+end
+
+function count_matches(p)
+    global part = p
+    rZero = Regex("^$(codes(0))\$")                 # regex for rule zero (exact match)
+    match(x) = length(collect(eachmatch(rZero, x))) # returns 1 if match
+    return sum(match.(messages))                    # count 1's
+end
+
+count_matches.([1,2])
